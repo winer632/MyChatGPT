c9952d3cf377f31c14e31fd5623ae1766d2782fc
wangxinxin <wangxinxin@sensetime.com>
2023-06-02 11:46:29 +0800

    add comment

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # MyChatGPT
+# business_type include basic_chat, internet_chat, pdf and so on
+# subscription_type include trial，per_month，per_year
+# access_key should be unique in the scope of business_type
 
 pip install -r requirements.txt
--- jwt_client.py
+++ jwt_client.py
@@ -7,10 +7,13 @@ url = "http://20.24.36.19:2023/v1/validity"
 data = {"type": "trial"} # or {"type": "subscription"}
 
 #change this to your api key and query if you are in your validity period
-ACCESS_KEY = "9RWmyu5BYQvKLvuRxpqm5k5hcpzE1U1DzcPrMkJbsdE="
+ACCESS_KEY = "jfwkgMx7xK8kZ0U4u2NZvd_-5iVdmRxtU5ux1DAKQ3o="
 
 # Define the secret key and the payload for JWT
 secret_key = "vs63TVu7HD_8ofiqBKZZ-D4sDqTo1003x05tS7o5j6c"
+# business_type include basic_chat, internet_chat, pdf and so on
+# subscription_type include trial，per_month，per_year
+# access_key should be unique in the scope of business_type
 payload = {
     "sub": ACCESS_KEY,
     "business_type": "basic_chat",
