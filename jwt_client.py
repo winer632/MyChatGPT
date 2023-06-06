6b9a30f0e74df672191c2e0fb296d3d417ab8bdf
wangxinxin <wangxinxin@sensetime.com>
2023-06-06 20:52:45 +0800

    modify jwt_client, remove jwt

--- jwt_client.py
+++ jwt_client.py
@@ -1,42 +1,27 @@
 # Import the modules
 import requests
-import jwt
-
-# Define the URL and the data
-# url = "http://20.24.36.19:2023/v1/validity"
-url = "https://service.bizoe.tech/v1/validity"
-data = {"type": "trial"} # or {"type": "subscription"}
 
 #change this to your api key and query if you are in your validity period
 ACCESS_KEY = "ajbieaef23245678"
+BASE_URL = "https://service.bizoe.tech/"
 
-# Define the secret key and the payload for JWT
-secret_key = "vs63TVu7HD_8ofiqBKZZ-D4sDqTo1003x05tS7o5j6c"
-# business_type include basic_chat, internet_chat, pdf and so on
-# subscription_type include trial，per_month，per_year
-# access_key should be unique in the scope of business_type
-payload = {
-    "sub": ACCESS_KEY,
-    "business_type": "basic_chat",
-    "iat": 1516239022
-}
-
-# Encode the payload and generate the token
-token = jwt.encode(payload, secret_key, algorithm="HS256")
-print("token is ", token)
-
-# Define the headers with the token
-headers = {
-    "Authorization": f"Bearer {token}"
-}
-
-# Send the POST request and get the response
-response = requests.post(url, json=data, headers=headers, verify=False)
-if response.status_code == 200 and response.headers['Content-Type'] == 'application/json':
-    # Print the response status code and data
-    print(response.status_code)
+# Define a method for testing the validity endpoint
+def test_validity():
+    # Define a payload with a valid access key
+    payload = {"access_key": "some_valid_id"}
+    # Make a POST request to the validity endpoint
+    response = requests.post(BASE_URL + "/v1/validity", json=payload, verify=False)
     print(response.json())
-else:
-    print("Something went wrong with the request")
-    print(response.text)
+    print(response.json()["validation"])
+    print(response.json()["message"])
 
+# Define a method for testing the recharge endpoint
+def test_recharge():
+    # Define a payload with a valid payment intent ID, amount and business model ID
+    payload = {"paymentIntentId": "some_valid_id", "amount": 1000, "business_model_id": 1}
+    # Make a POST request to the recharge endpoint
+    response = requests.post(BASE_URL + "/v1/recharge", json=payload, verify=False)
+    print(response.json())
+
+test_validity()
+test_recharge()
