7751355a2b075724a40991477811d0de48925105
wangxinxin <wangxinxin@sensetime.com>
2023-06-08 21:02:49 +0800

    CORS

--- jwt_server.py
+++ jwt_server.py
@@ -82,7 +82,7 @@ def recharge():
         # Set the CORS headers
         response.headers["Access-Control-Allow-Origin"] = "*"
         response.headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, OPTIONS, DELETE"
-        response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+        response.headers["Access-Control-Allow-Headers"] = "content-type"
         # Return the response object
         return response
 
@@ -101,7 +101,7 @@ def recharge():
     # Set the CORS headers
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, OPTIONS, DELETE"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    response.headers["Access-Control-Allow-Headers"] = "content-type"
     # Return the response object
     return response
 
