e17a974f5dd18ad26360191df49d3ffc8eb3e5ac
wangxinxin <winer632@qq.com>
2023-06-21 09:18:59 +0800

    有效期查询接口和鉴权接口分开

--- jwt_server.py
+++ jwt_server.py
@@ -107,3 +107,63 @@ def recharge():
     # Return the response object
     return response
 
+
+# Define the validity endpoint
+@app.route("/v1/settings/validity", methods=["POST", "OPTIONS"])
+def validity():
+    # Check if the request is an OPTIONS request
+    if request.method == "OPTIONS":
+        # Create an empty response object
+        response = flask.Response("")
+        # Set the CORS headers
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        response.headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, OPTIONS, DELETE"
+        response.headers["Access-Control-Allow-Headers"] = "content-type"
+        # Return the response object
+        return response
+
+    # Get the payload as a JSON object
+    payload = request.get_json()
+    access_key = payload["access_key"]
+    print("access_key is ", access_key)
+    # Create connection and cursor objects
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="123456",
+        database="gpt"
+    )
+    cursor = connection.cursor(dictionary=True)
+
+    sql = "SELECT expiration_date FROM account USE INDEX (access_key_expiration_date) WHERE expiration_date > NOW() AND access_key = %s"
+    val = (access_key,)
+    cursor.execute(sql, val)
+
+
+    # Fetch the result
+    row = cursor.fetchone()
+    # Commit the changes to the database and close the cursor and connection objects
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    # Check if there are any rows in the result set
+    if row is not None:
+        expiration_date = row["expiration_date"]
+        # Create a response object
+        response = flask.jsonify({"validation":"success", "message":"valid until "+expiration_date.strftime("%Y-%m-%d %H:%M:%S")})
+        # Set the CORS headers
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        response.headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, OPTIONS, DELETE"
+        response.headers["Access-Control-Allow-Headers"] = "content-type"
+        # Return the response object
+        return response
+    else:
+        # Create a response object
+        response = flask.jsonify({"validation": "fail", "message": "No valid subscription found"})
+        # Set the CORS headers
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        response.headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, OPTIONS, DELETE"
+        response.headers["Access-Control-Allow-Headers"] = "content-type"
+        # Return the response object
+        return response
